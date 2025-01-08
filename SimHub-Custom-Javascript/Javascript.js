fa8f1171d115f326501fd94e28b6cd2d5c11db45
Jordessey <96155508+Jordessey@users.noreply.github.com>
2025-01-09 12:32:39 +1300

    Update Javascript.js

--- SimHub-Custom-Javascript/Javascript.js
+++ SimHub-Custom-Javascript/Javascript.js
@@ -14,26 +14,27 @@ if ($prop("GameRawData.Telemetry.SessionFlagsDetails.Isblack") || $prop("GameRaw
 	BLKDSQ = 0;
 	}
 
-if ($prop('Flag_Blue') == true){
-	Flag = 8;
-} else if ($prop("GameRawData.Telemetry.SessionFlagsDetails.IscautionWaving") == true){
+
+if ($prop("GameRawData.Telemetry.SessionFlagsDetails.IscautionWaving") == true){
 	Flag = 6;
 } else if ($prop("GameRawData.Telemetry.SessionFlagsDetails.Iscaution") == true){
 	Flag = 7;
+} else if ($prop('Flag_Blue') == true){
+	Flag = 8;
 } else if ($prop("GameRawData.Telemetry.SessionFlagsDetails.Isred") == true){
 	Flag = 5;
+} else if ($prop("GameRawData.Telemetry.SessionFlagsDetails.Isrepair") == true){
+	Flag = 3;
+} else if ($prop("GameRawData.Telemetry.SessionFlagsDetails.IsstartSet") == true){
+	Flag = 9;
+} else if ($prop("GameRawData.Telemetry.SessionFlagsDetails.Isgreen") == true){
+	Flag = 2;
 } else if ($prop("GameRawData.Telemetry.SessionFlagsDetails.Iswhite") == true){
 	Flag = 4;
 } else if ($prop("GameRawData.Telemetry.SessionFlagsDetails.Ischeckered") == true){
 	Flag = 1;
-} else if ($prop("GameRawData.Telemetry.SessionFlagsDetails.Isrepair") == true){
-	Flag = 3;
-} else if ($prop("GameRawData.Telemetry.SessionFlagsDetails.Isgreen") == true){
-	Flag = 2;
-}else if ($prop("GameRawData.Telemetry.SessionFlagsDetails.IsstartSet") == true){
-	Flag = 9;
-}else {
-	Flag = 0
+} else {
+Flag = 0
 }
 
 
